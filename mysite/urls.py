0b10328c8a7c6ba143b88c65465cc7700b801b0a
Yohan Ysturiz <yohanysturiz@gmail.com>
2018-01-19 09:56:08 -0400

    Cambios en el html de mi pagina

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # cuando se escriben expresiones regulares en phyton es importante colocar la letra r al principio, funciona como pista
+    # para python y que este entienda que la cadena contendra caracteres especiales.
+    
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
